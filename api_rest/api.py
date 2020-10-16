145e118ca2e11152026f95d5fa79093a772ac741
Damián Pérez Alfonso <dperez@geocom.com.uy>
2020-10-16 17:52:59 -0300

    Add argument Parsing

--- api_rest/api.py
+++ api_rest/api.py
@@ -1,11 +1,17 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
-
+from flask_restful import Api, Resource, reqparse, fields, marshal_with
 
 app = Flask(__name__)
 api = Api(app)
 
 orders = {}
+
+resource_fields = {
+    'order_id':   fields.String,
+    'prediction':   fields.Integer,
+    'confidence':   fields.Float
+}
+
 
 class PredictToTakeOrder(Resource):
 
@@ -16,28 +22,29 @@ class PredictToTakeOrder(Resource):
     def get(self):
         return orders
 
+    @marshal_with(resource_fields)
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('order_id')
-        parser.add_argument('store_id')
-        parser.add_argument('to_user_distance')
-        parser.add_argument('to_user_elevation')
-        parser.add_argument('total_earning')
-        parser.add_argument('created_at')
+        parser.add_argument('order_id', type=str, help='Order ID')
+        parser.add_argument('store_id', type=str, help='Store ID of the order')
+        parser.add_argument('to_user_distance', type=float, help='Distance (km) between store and user location')
+        parser.add_argument('to_user_elevation', type=float, help='Difference (m) between the store and user altitude')
+        parser.add_argument('total_earning', type=float, help='Courier earning by delivering the order')
+        parser.add_argument('created_at', type=str, help='Timestamp of order creation')
 
-        args = parser.parse_args()
+        # strict=True ensures that an error is thrown if the request includes arguments your parser does not define
+        args = parser.parse_args(strict=True)
 
-        prediction=1
-        confidence=0.9
+        prediction = 1
+        confidence = 0.9
 
         args['prediction'] = prediction
         args['confidence'] = confidence
 
-        output = {'order_id': args['order_id'], 'prediction': prediction, 'confidence': confidence}
-
         orders[args['order_id']] = args
 
-        return output
+        return args
+
 
 api.add_resource(PredictToTakeOrder, '/toTake')
 
